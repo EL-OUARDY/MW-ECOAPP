011563071d050ac3f154c2358dd404f718104f98
ouadie09 <edy.developer09@gmail.com>
2019-02-01 23:03:21 +0000

    add auto mapper

---
+++ MW-Backend/App_Start/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MW_Backend.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MW_Backend.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+           // Mapper.CreateMap<Product, ProductDTO>();
+           // Mapper.CreateMap<ProductDTO, Product>();
+        }
+    }
+}
